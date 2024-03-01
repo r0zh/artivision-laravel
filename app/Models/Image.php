f166f8af33e0fc771f6a163422b3d0542f1e6843
r0zh <rozh.software.engineer17@gmail.com>
2024-03-01 16:52:50 +0100

    add Image table

---
+++ app/Models/Image.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    use HasFactory;
+
+    protected $table = 'image';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'user_id', // 'user_id' is a foreign key to the 'id' column in the 'users' table
+        'name',
+        'path',
+        'positivePrompt',
+        'negativePrompt',
+    ];
+}
