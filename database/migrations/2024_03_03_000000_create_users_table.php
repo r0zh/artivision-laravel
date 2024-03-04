b5586739c320c5e2c908a0c3b6283c6171dcfe0b
r0zh <r0zh017@proton.me>
2024-03-04 12:51:10 +0100

    add default value to user role

--- database/migrations/2024_03_03_000000_create_users_table.php
+++ database/migrations/2024_03_03_000000_create_users_table.php
@@ -8,10 +8,11 @@ return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void {
+    public function up(): void
+    {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('role_id');
+            $table->unsignedBigInteger('role_id')->default(1);
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
@@ -25,7 +26,8 @@ return new class extends Migration {
     /**
      * Reverse the migrations.
      */
-    public function down(): void {
+    public function down(): void
+    {
         Schema::dropIfExists('users');
     }
 };
