a709bf2ea2d271753eafb8ad06c284dd85daa523
r0zh <rozh.software.engineer17@gmail.com>
2024-03-04 10:43:10 +0100

    fix foreign key errors

--- database/migrations/2024_03_04_114139_create_image_table.php
+++ database/migrations/2024_03_04_114139_create_image_table.php
@@ -4,13 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('image', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
@@ -20,15 +18,14 @@ return new class extends Migration
             $table->text('negativePrompt');
             $table->boolean('public');
             $table->timestamps();
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('image');
     }
 };
