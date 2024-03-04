ef99fc95cf1978042b1da810b2678ced0d5604e3
r0zh <rozh.software.engineer17@gmail.com>
2024-03-04 02:13:09 +0100

    remove unused ModalComponent

--- app/Livewire/Gallery.php
+++ app/Livewire/Gallery.php
@@ -6,52 +6,60 @@ use App\Models\Image;
 use Livewire\Attributes\On;
 use Livewire\Component;
 
-class Gallery extends Component {
+class Gallery extends Component
+{
     public $images;
 
     public $filter = 'all';
 
     public $selectedImage = null;
+
     public $filteredImages;
 
-    public function mount() {
-        $this->images         = Image::where('user_id', auth()->id())->get();
+    public function mount()
+    {
+        $this->images = Image::where('user_id', auth()->id())->get();
         $this->filteredImages = $this->images;
     }
 
     protected $listeners = ['filterUpdated' => 'updateFilterVisibility'];
 
-    public function updateFilterVisibility($filter, $force = false) {
+    public function updateFilterVisibility($filter, $force = false)
+    {
         if ($filter != $this->filter || $force) {
 
-            $this->filter         = $filter;
+            $this->filter = $filter;
             $this->filteredImages = match ($filter) {
-                'all'     => $this->images,
-                'public'  => $this->images->where('public', true),
+                'all' => $this->images,
+                'public' => $this->images->where('public', true),
                 'private' => $this->images->where('public', false),
-                default   => $this->images,
+                default => $this->images,
             };
         }
     }
 
     #[On('imageDeleted')]
-    public function imageDeleted() {
+    public function imageDeleted()
+    {
         $this->images = Image::where('user_id', auth()->id())->get();
         $this->updateFilterVisibility($this->filter, true);
     }
 
     #[On('imageVisibilityUpdated')]
-    public function imageVisibilityUpdated() {
+    public function imageVisibilityUpdated()
+    {
         $this->images = Image::where('user_id', auth()->id())->get();
         $this->updateFilterVisibility($this->filter, true);
     }
 
     #[On('imageSelected')]
-    public function imageSelected($id) {
-        $this->dispatch('openModal', "ImageDetails", ['id' => $id]);
+    public function imageSelected($id)
+    {
+        $this->dispatch('openModal', 'ImageDetails', ['id' => $id]);
     }
 
-    public function render() {
+    public function render()
+    {
         return view('livewire.artivision.gallery');
     }
 }
