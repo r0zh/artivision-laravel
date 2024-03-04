4a43798a518e4fec78182238f076701d5ca64f5f
r0zh <rozh.software.engineer17@gmail.com>
2024-03-04 10:45:03 +0100

    remove unused varaibles

--- app/Livewire/Gallery.php
+++ app/Livewire/Gallery.php
@@ -6,8 +6,7 @@ use App\Models\Image;
 use Livewire\Attributes\On;
 use Livewire\Component;
 
-class Gallery extends Component
-{
+class Gallery extends Component {
     public $images;
 
     public $filter = 'all';
@@ -16,25 +15,15 @@ class Gallery extends Component
 
     public $search;
 
-    public $orderedImages;
-
-    public $searchedImages;
-
-    public $filteredImages;
 
     public $direction = 'desc';
 
-    public function mount()
-    {
+    public function mount() {
         $this->images = Image::where('user_id', auth()->id())->get();
-        $this->filteredImages = $this->images;
-        $this->searchedImages = $this->filteredImages;
-        $this->orderedImages = $this->searchedImages->sortByDesc('created_at');
     }
 
     #[On('filterUpdated')]
-    public function updateFilterVisibility($filter)
-    {
+    public function updateFilterVisibility($filter) {
         if ($filter != $this->filter) {
             $this->filter = $filter;
             $this->getImages();
@@ -42,8 +31,7 @@ class Gallery extends Component
     }
 
     #[On('searchUpdated')]
-    public function updateSearch($search)
-    {
+    public function updateSearch($search) {
         if ($search != $this->search) {
             $this->search = $search;
             $this->getImages();
@@ -52,56 +40,51 @@ class Gallery extends Component
     }
 
     #[On('orderUpdated')]
-    public function updateOrder($direction)
-    {
+    public function updateOrder($direction) {
         if ($direction != $this->direction) {
             $this->direction = $direction;
             $this->getImages();
         }
     }
 
-    public function getImages()
-    {
+    public function getImages() {
         if ($this->filter == 'all') {
             $this->images = Image::where('user_id', auth()->id())
-                ->where('positivePrompt', 'like', '%'.$this->search.'%')
+                ->where('positivePrompt', 'like', '%' . $this->search . '%')
                 ->orderBy('created_at', $this->direction)
                 ->get();
         } elseif ($this->filter == 'public') {
             $this->images = Image::where('user_id', auth()->id())
                 ->where('public', true)
-                ->where('positivePrompt', 'like', '%'.$this->search.'%')
+                ->where('positivePrompt', 'like', '%' . $this->search . '%')
                 ->orderBy('created_at', $this->direction)
                 ->get();
         } elseif ($this->filter == 'private') {
             $this->images = Image::where('user_id', auth()->id())
                 ->where('public', false)
-                ->where('positivePrompt', 'like', '%'.$this->search.'%')
+                ->where('positivePrompt', 'like', '%' . $this->search . '%')
                 ->orderBy('created_at', $this->direction)
                 ->get();
         }
+        dump($this->images);
     }
 
     #[On('imageDeleted')]
-    public function imageDeleted()
-    {
+    public function imageDeleted() {
         $this->getImages();
     }
 
     #[On('imageVisibilityUpdated')]
-    public function imageVisibilityUpdated()
-    {
+    public function imageVisibilityUpdated() {
         $this->getImages();
     }
 
     #[On('imageSelected')]
-    public function imageSelected($id)
-    {
+    public function imageSelected($id) {
         $this->dispatch('openModal', 'ImageDetails', ['id' => $id]);
     }
 
-    public function render()
-    {
+    public function render() {
         return view('livewire.artivision.gallery');
     }
 }
