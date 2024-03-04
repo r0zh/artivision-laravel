4ce921a8e726c35d373ada4061e4dbcfb2088ea3
r0zh <rozh.software.engineer17@gmail.com>
2024-03-04 06:30:54 +0100

    add missing field

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Image extends Model
-{
+class Image extends Model {
     use HasFactory;
 
     protected $table = 'image';
@@ -15,6 +14,7 @@ class Image extends Model
 
     protected $fillable = [
         'user_id', // 'user_id' is a foreign key to the 'id' column in the 'users' table
+        'seed',
         'name',
         'path',
         'positivePrompt',
@@ -22,8 +22,7 @@ class Image extends Model
         'public',
     ];
 
-    public function user()
-    {
+    public function user() {
         return $this->belongsTo(User::class)->first();
     }
 }
